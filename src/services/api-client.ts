0203194336c12b8b1636d08ec54196077732e7a3
Davi <davim.lira@gmail.com>
2025-04-17 12:37:19 -0500

    Fix apiKey

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "709fe62502df44f4b0317e9a87111e64",
   },
 });
